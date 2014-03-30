9476949330b35532e324ac5c57aadae340b1a6f4
sunghwano <sunghwano@NOH>
2014-03-30 22:47:16 +0900

    .

--- www/js/index.js
+++ www/js/index.js
@@ -16,130 +16,189 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+	
 $(document).ready(function(){
 
-
-	var p = ["허태량" , "한태희" , "김승범" , "송재우" , "김성호" , "유형덕" , "박용범" , "노성화" , "황지선" , "이혜주" , 
-           "강성진" , "좌행복" , "한창수" , "이승진" , "오송이" , "이강진" , "차규원" , "정두리" , "이현주" , "전영범" ,
-           "서동일" , "최일환" , "김세미" , "유승범" , "구상현" , "이유라" , "박헌웅" , "김종현" , "석지오" , "노의현"];
-	var color = ["red" , "orange" , "yellow" , "green" , "blue" , "violet" , "white" , "brown" , "skyblue" , "gray" , "lightgreen" , "silver" , "pink" , "gold" , "wine"];
-	var color2 = ["red", "black"];
-	var interval;
-	var who;
-	var input;
-
-
-	css();
-
-
-
-	$("#btn").click(function(){
-		start();
+	var CheckMember = [];
+	var NoCheckMember = [];
+	var Couple = [];
+	var SaveCount;
+	var WhoCheck;
+	var WhoNoCheck;
+	var Count = 0;
+	var AddTextCount = 0;
+	var RandomCountMinus = 0;
+	var BounceReturn = 0;
+	var BounceCount = 0;
+	var BounceReturn2 = 0;
+	var BounceCount2 = 0;
+	
+	Bounce();
+	
+	$("#get").click(function(){
+		 Check(); 
 	});
-
-
-
-
-
-
-	function start(){
-		document.getElementById("h1").innerHTML = "과연~~누가??";
 	
-		$("#btn").css({
-			"display" : "none",
-			"background-colir" : "white"
-		});
-		
-		input = 3;
-
-		interval = setInterval(function(){
-			
-			$("#content1").css({
-				"font-size" : "100px"
-			});
-			document.getElementById("content1").innerHTML = input--;
-		}, 1000);
-
-		setTimeout(function(){
-			clearInterval(interval);
-			$("#btn").css({
-				"display" : "none"
-			});
-
-			
-			
-
-			bounse();
-		
-		}, (input + 1) * 1000);
-	}
-
-
-	function css(){
-		setInterval(function(){
-
-			$("#h1").css({
-				"color" : color[parseInt(Math.random() * 30)]
-			});
-
-			
-			$("#content1").css({
-				"color" : "red"
-			});
-			
-			$("#st").css({
-				"color" : color[parseInt(Math.random() * 30)]
-			});
-
-		}, 50);
-	}
-
+	$("#shuffle").click(function(){
+	   Random(); 
+	});
 	
-	function bounse(){
-		var i = 0;
-		var j = 0;
-		var k = 0;
-		var input = 30;
-		var size = 80;
-		setInterval(function(){
-			who = p[parseInt(Math.random() * 30)];
-			
-			if(k==0){
-			document.getElementById("h1").innerHTML = who;
-			document.getElementById("content1").innerHTML = input--;
-			$("#content1").css({
-				"font-size" :  size -= 1
-			});
-			}
-			
-			if(i==0){
-				$("#h1").css({
-					"font-size" : j += 5
-				});
-				if(j == 150){
-				i += 1;
-				}
-				}
-
-			if(k==0 && j==150){
-				
-				document.getElementById("h1").innerHTML = who + "님 축하합니다~";
-				document.getElementById("content1").innerHTML = "";
-				$("#btn").css({
-					"display" : ""
-				});
-				k++;
-			}
-			
-			
-			
-		}, 600);
+	$("#Cancel").click(function(){
+		Clear();
+	  Reset();
+	});
+	
+	function Check(){
 		
-		
+		SaveCount = 0;
+	    
+    $("input[name=box]").attr("class" , 1);
+    
+    $("input[name=box]:checked").attr("class", 2).each(function(){
+      CheckMember[SaveCount++] = $(this).val();
+    });
+    
+    if(CheckMember.length == 15){
+			document.getElementById("Who").innerHTML = "";
+			Checked();
+    	NoCheck();
+    }else if(CheckMember.length > 15){
+    	alert(CheckMember.length + "명 체크하셨습니다." + (CheckMember.length - 15) + "명 체크 해제해주세요");
+    	CheckMember.splice(0, CheckMember.length);
+    }else{
+      alert(CheckMember.length + "명 체크하셨습니다." + (15 - CheckMember.length) + "명 더 체크해주세요.");
+      CheckMember.splice(0, CheckMember.length);
+    }
 	}
 	
+	function Checked(){
+	    
+    CheckMember.forEach(function(value){
+       $("#CheckName").append($('<p></p>').text(CheckMember[AddTextCount++]));
+    });
+    AddTextCount = 0;
+  }
+	
+  function NoCheck() {
 
-
-
-
+    SaveCount = 0;
+    
+    $(".1").each(function(){
+      NoCheckMember[SaveCount++] = $(this).val();
+    }); 
+    NoChecked();
+    
+    $("input[name=box]").attr("checked" , false);
+    $("#get").css("display","none");
+    $("#shuffle").css("display","");
+    $("#Cancel").css("display","");
+  }
+  
+  function NoChecked(){
+      
+    NoCheckMember.forEach(function(value){
+       $("#NoCheckName").append($('<p></p>').text(NoCheckMember[AddTextCount++]));
+    });
+    AddTextCount = 0;
+    document.getElementById("TeamList").innerHTML = "";  
+  }
+  
+  function Random(){
+	    
+    Clear();
+    
+    if(Count < 15){
+    
+      WhoCheck = parseInt(Math.random() * (15 - RandomCountMinus));
+      WhoNoCheck = parseInt(Math.random() * (15 - RandomCountMinus));
+      
+      $("#Who").append($('<span></span>').html((RandomCountMinus + 1) + "팀<br>").css("color" , "black"))
+      		   .append($('<span></span>').html(CheckMember[WhoCheck]).css("color" , "blue"))
+               .append($('<span></span>').html("  ♥  " + NoCheckMember[WhoNoCheck]).css("color" , "red"));
+      
+      Couple[RandomCountMinus++] = CheckMember[WhoCheck] + "  ♥  " + NoCheckMember[WhoNoCheck];
+      
+      CheckMember.splice(WhoCheck , 1);
+      NoCheckMember.splice(WhoNoCheck , 1);
+      
+      document.getElementById("CheckName").innerHTML = "";
+      document.getElementById("NoCheckName").innerHTML = "";
+      
+      Checked();
+      NoChecked();
+      CoupleList();
+      
+      Count++;
+      
+      if(Count == 15){
+        alert("끝");
+        document.getElementById("Who").innerHTML = "축하합니다~^^";
+        
+        Reset();
+      }
+    }
+  }
+		  
+  function CoupleList(){
+    
+     Couple.forEach(function(value){
+        $("#TeamList").append($('<span></span>').html((AddTextCount + 1) + "팀 : " + Couple[AddTextCount++] + "<br>"));
+     });
+     AddTextCount = 0;
+  }
+	
+	function Clear(){
+		 document.getElementById("Who").innerHTML = "";
+		 document.getElementById("TeamList").innerHTML = "";
+     document.getElementById("CheckName").innerHTML = "";
+     document.getElementById("NoCheckName").innerHTML = "";
+	}
+	
+	function Reset() {
+		Count = 0;
+		AddTextCount = 0;
+    RandomCountMinus = 0;
+    CheckMember.splice(0, CheckMember.length);
+    Couple.splice(0, Couple.length);
+    $("#get").css("display","");
+    $("#shuffle").css("display","none");
+    $("#Cancel").css("display","none");
+	}
+	
+  function Bounce(){
+    
+   var input = setInterval(function() {
+    
+    if (BounceReturn == 0) {
+      $("#Love").css("font-size", BounceCount += 3);
+      if (BounceCount == 300) {
+    	  BounceReturn = 1;
+      }
+    }
+    
+    if (BounceReturn == 1) {
+      $("#Love").css("font-size", BounceCount -= 3);
+        if (BounceCount == 0) {
+        	BounceReturn = 0;
+        }
+      }
+    }, 30);
+   
+   var input2 = setInterval(function() {
+   
+   if (BounceReturn2 == 0) {
+      $("#Who").css("font-size", BounceCount2 += 1);
+      if (BounceCount2 == 80) {
+        BounceReturn2 = 1;
+      }
+    }
+    
+    if (BounceReturn2 == 1) {
+      $("#Who").css("font-size", BounceCount2 -= 1);
+        if (BounceCount2 == 40) {
+          BounceReturn2 = 0;
+        }
+      }
+    }, 30);
+  }
 });
